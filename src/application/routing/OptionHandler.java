88d265ce8891c095ccd16c797621dcb34d30f108
Luca <marchisioluca15@gmail.com>
2024-01-16 22:37:12 +0100

    [Delete] deleted useless "STR" class and moved "OptionHandler" class.

--- src/application/routing/OptionHandler.java
+++
@@ -1,9 +0,0 @@
-package application.routing;
-
-import application.http.structure.request.Request;
-import application.http.structure.response.Response;
-
-@FunctionalInterface
-public interface OptionHandler {
-    void handle(Request request, Response response);
-}
