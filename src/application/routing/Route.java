ba39a91c8407d117c2f7f34638391562840f7d5f
Luca <marchisioluca15@gmail.com>
2024-01-16 22:50:39 +0100

    [Fix] Route constructor logic

--- src/application/routing/Route.java
+++ src/application/routing/Route.java
@@ -1,19 +1,30 @@
 package application.routing;
 
 import application.http.structure.request.RequestMethod;
+import application.options.OptionHandler;
 import application.routing.config.EndpointMapper;
+import logger.Logger;
 import reflection.AnnotationFinder;
 
-import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class Route implements AnnotationFinder<Method> {
-    private final String path;
+    private String path;
     private final ArrayList<Endpoint> endPoints;
 
-    public Route(Class<?> routeClass) {
-        path = File.separator + routeClass.getPackageName().replace('.', File.separatorChar);
+    public Route(Class<?> routeClass) throws IOException {
+        path = "/" + routeClass.getPackageName().replaceAll("\\.", "/");
+
+        // Check if the "/routes" string is present; if so remove it else throw an error.
+        if (path.contains("/routes")) {
+            // If it's just the route path replace it with just '/', if not
+            // don't add a double '/'
+            if (path.equals("/routes")) path = path.replaceAll("/routes", "/");
+            else path = path.replaceAll("/routes", "");
+        }
+        else throw new IOException("Malformed path! " + routeClass.getName() + " should be in the root directory '/routes'.");
 
         endPoints = new ArrayList<>();
 
@@ -22,11 +33,15 @@ public class Route implements AnnotationFinder<Method> {
         for (var m : valid) endPoints.add(new Endpoint(m));
     }
 
-    public Route(String path, OptionHandler handler, RequestMethod requestMethod) throws NoSuchMethodException {
+    public Route(String path, OptionHandler handler, RequestMethod requestMethod) {
         this.path = path;
-
         endPoints = new ArrayList<>();
-        endPoints.add(new Endpoint(handler, requestMethod));
+
+        try {
+            endPoints.add(new Endpoint(handler, requestMethod));
+        } catch (NoSuchMethodException e) {
+            Logger.err(e);
+        }
     }
 
     public String getPath() {
@@ -39,9 +54,9 @@ public class Route implements AnnotationFinder<Method> {
 
     @Override
     public String toString() {
-        return "RouteInfo{" +
+        return "Route{" +
                 "path='" + path + '\'' +
-                ", endPointsInfo=" + endPoints +
+                ", endPoints=" + endPoints +
                 '}';
     }
 }
